80f1e1f9f0094ace05d20c3b4299b0d8197dfcb5
RealGamerDoge <75544158+RealGamerDoge@users.noreply.github.com>
2020-12-22 11:46:59 -0500

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,98 @@
+clear()
+
+//SETUP CODE! MAKE SURE TO COMPLETE IF YOU DO NOT THIS SCRIPT WILL NOT WORK!!!
+var yourAccount = ""; //Enter your account page here
+
+//This will be ways to reach people
+var suggestedAccountsFollowers = ""; //Put someone else's profile here. Make sure that you both post similar stuff. 
+var hashTag = ""; //Hashtags of the same style will get you more fans
+
+//Script Registration
+var serialKey = ""; //And finally to register your script. Please enter your serial key here.
+
+
+console.log("░░░░░░░░░▄░░░░░░░░░░░░░░▄░░░░")
+console.log("░░░░░░░░▌▒█░░░░░░░░░░░▄▀▒▌░░░")
+console.log("░░░░░░░░▌▒▒█░░░░░░░░▄▀▒▒▒▐░░░")
+console.log("░░░░░░░▐▄▀▒▒▀▀▀▀▄▄▄▀▒▒▒▒▒▐░░░")
+console.log("░░░░░▄▄▀▒░▒▒▒▒▒▒▒▒▒█▒▒▄█▒▐░░░")
+console.log("░░░▄▀▒▒▒░░░▒▒▒░░░▒▒▒▀██▀▒▌░░░")
+console.log("░░▐▒▒▒▄▄▒▒▒▒░░░▒▒▒▒▒▒▒▀▄▒▒▌░░")
+console.log("░░▌░░▌█▀▒▒▒▒▒▄▀█▄▒▒▒▒▒▒▒█▒▐░░")
+console.log("░▐░░░▒▒▒▒▒▒▒▒▌██▀▒▒░░░▒▒▒▀▄▌░")
+console.log("░▌░▒▄██▄▒▒▒▒▒▒▒▒▒░░░░░░▒▒▒▒▌░")
+console.log("▀▒▀▐▄█▄█▌▄░▀▒▒░░░░░░░░░░▒▒▒▐░")
+console.log("▐▒▒▐▀▐▀▒░▄▄▒▄▒▒▒▒▒▒░▒░▒░▒▒▒▒▌")
+console.log("▐▒▒▒▀▀▄▄▒▒▒▄▒▒▒▒▒▒▒▒░▒░▒░▒▒▐░")
+console.log("░▌▒▒▒▒▒▒▀▀▀▒▒▒▒▒▒░▒░▒░▒░▒▒▒▌░")
+console.log("░▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒░▒░▒░▒▒▄▒▒▐░░")
+console.log("░░▀▄▒▒▒▒▒▒▒▒▒▒▒░▒░▒░▒▄▒▒▒▒▌░░")
+console.log("░░░░▀▄▒▒▒▒▒▒▒▒▒▒▄▄▄▀▒▒▒▒▄▀░░░")
+console.log("░░░░░░▀▄▄▄▄▄▄▀▀▀▒▒▒▒▒▄▄▀░░░░░")
+console.log("░░░░░░░░░▒▒▒▒▒▒▒▒▒▒▀▀░░░░░░░░")
+console.log("")
+console.log("Made by UnfunnyDoge. Contact on Discord: UnfunnyDoge#2939")
+console.log("")
+console.log("Make sure that the setup code is up!")
+
+var serialCode = "vju-sd79eb-cu8-e8w9vb-c";
+
+if (serialKey == serialCode ) {
+
+    alert("The serial code is in and correct!")  
+}
+
+else{ 
+     alert("Sorry. The serial code is incorrect. If you need to purchase one please contact me on Discord: UnfunnyDoge#2939")      
+        
+        clear()
+        document.location.reload ()
+}
+
+
+
+if (serialKey == serialCode ) {
+    alert("The serial code is in and correct!")        
+
+}
+
+
+window.location.href = suggestedAccountsFollowers;
+
+
+(() => {
+    let i = 0;
+    const followInterval = setInterval(() => {
+        if (i >= 30) {
+            clearInterval(followInterval);
+            return;
+        }
+        const buttons = document.querySelectorAll('button')parentNode;
+        const nextButton = buttons[i];
+        nextButton.click();
+        i += 1;
+    }, 500)
+})()
+
+window.location.href = hashTag;
+
+const startHashTag = document.querySelector('v1Nh3 kIKUG  _bz0w');
+startHashTag.click();
+
+let likes = 0;
+setInterval(() => {
+    const heart = document.querySelector('svg[aria-label="Like"]');
+    const arrow = document.querySelector('a.coreSpriteRightPaginationArrow');
+    if (heart) {
+        heart.click()
+        likes++;
+        console.log(`You've liked ${likes} post(s)`);
+    }
+    arrow.click();
+}, 5000);
+
+
+
+
+
+
